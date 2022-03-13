3964a36a652a204842c4a2a9bcee040d5c0a9842
arnobchakma <arnobchakma05@gmail.com>
2022-03-13 04:23:32 -0500

    firebase deploy

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASzgHen4NRSs0EYHIH76RmuJyWpHoZ6Cs",
+    authDomain: "react-cryptocurrency-c3f54.firebaseapp.com",
+    projectId: "react-cryptocurrency-c3f54",
+    storageBucket: "react-cryptocurrency-c3f54.appspot.com",
+    messagingSenderId: "361221197237",
+    appId: "1:361221197237:web:ed757c7446b622ec13ddfc"
+};
+
+export default firebaseConfig;
